e9ba5a2c604067540dd29371d52397043717ce46
Isaac Helbling <imhelbling@gmail.com>
2017-01-25 00:53:19 -0500

    it sort of does the thing now!

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var jetpack = require('fs-jetpack')
+var program  = require('commander')
+var jetpack  = require('fs-jetpack')
+var userHome = require('user-home')
+var yaml     = require('js-yaml')
+var git      = require('simple-git')(jetpack.cwd())
+
+
 
 program
-.arguments('<type> <name>')
-.option('-g, --git', 'initialize the project as a Git repository')
-.action(function(type, name) {
-  console.log(type, name)
-})
-.parse(process.argv);
+
+  .arguments('<type> <path>')
+
+  .option('-g, --git', 'initialize the project as a Git repository')
+
+  .action(function(type, path) {
+    var soupfilePath = userHome+'/soupfile.yml'
+    var soupfile     = yaml.safeLoad(jetpack.read(soupfilePath))
+    var noodles      = soupfile.noodles
+    var noodle       = noodles[type]
+
+    git.clone(noodle, path)
+
+  })
+
+  .parse(process.argv)
