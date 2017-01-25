9ff94603fbee8bc934d7a5553ecbf2f58f78a148
Isaac Helbling <imhelbling@gmail.com>
2017-01-25 03:21:49 -0500

    developed command structure

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 #!/usr/bin/env node
 
-var program  = require('commander')
+var niru     = require('commander')
 var jetpack  = require('fs-jetpack')
 var userHome = require('user-home')
 var yaml     = require('js-yaml')
 var git      = require('simple-git')(jetpack.cwd())
 
+var soupfilePath   = `${userHome}/soupfile.yml`
+var soupfileExists = jetpack.exists(soupfilePath)
 
 
-program
 
-  .arguments('<type> <path>')
-
-  .option('-g, --git', 'initialize the project as a Git repository')
-
-  .action(function(type, path) {
-    var soupfilePath = userHome+'/soupfile.yml'
-    var soupfile     = yaml.safeLoad(jetpack.read(soupfilePath))
-    var noodles      = soupfile.noodles
-    var noodle       = noodles[type]
-
-    git.clone(noodle, path)
-
+niru
+  // .usage('init')
+  .command('init', 'initialize niru with a soupfile')
+  .action(function(){
+    if (niru.args[0] === 'init') {
+      console.log(`created soupfile at ${soupfilePath}`)
+      process.exit()
+    }
   })
 
-  .parse(process.argv)
+niru
+  // .usage('<noodle> [path]')
+  .arguments('<noodle> [path]')
+  .option('-g, --git', ' initialize the project as a Git repository')
+  .action(function(chopstick, path){
+    var soup         = yaml.safeLoad(jetpack.read(soupfilePath))
+    var noodles      = soup.noodles
+    var noodle       = noodles[chopstick]
+    var repoName     = noodle.split('/')[4]
+    if (!path) var path = repoName
+
+    console.log(`creating ${chopstick} boilerplate at ${path}`)
+    git.clone(noodle, (jetpack.cwd() + '/' + path) || jetpack.cwd() + '/' + repoName)
+    process.exit()
+  })
+
+niru.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ niru -g my-boilerplate ./new-project');
+  console.log('');
+});
+
+niru.usage(
+  /*      niru*/'[options] <noodle> [path]\n' +
+  '     Or: niru <command> [options]'
+)
+
+niru.parse(process.argv)
