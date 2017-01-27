9cadd18ec5e3dc8c740d81bf74b22a24774717f0
Isaac Helbling <imhelbling@gmail.com>
2017-01-27 12:49:45 -0500

    removed commander stuff from index.js

--- index.js
+++ index.js
@@ -9,37 +9,8 @@ var git      = require('simple-git')(jetpack.cwd())
 var soupfilePath   = `${userHome}/soupfile.yml`
 var soupfileExists = jetpack.exists(soupfilePath)
 
+var nodePath = process.argv[0]
+var niruPath = process.argv[1]
+var args     = process.argv.slice(2, process.argv.length)
 
-
-niru
-  .command('init', 'initialize niru with a soupfile')
-  .action(function(){
-    if (niru.args[0] === 'init') {
-      console.log(`created soupfile at ${soupfilePath}`)
-      process.exit()
-    }
-  })
-
-niru
-  .command('get', 'get a boilerplate from the collection')
-  .arguments('<boilerplate>')
-  .action(function(boilerplate){
-    if (niru.args[0] === 'get') {
-      var soup         = yaml.safeLoad(jetpack.read(soupfilePath))
-      var noodles      = soup.noodles
-      var noodle       = noodles[boilerplate]
-      console.log(
-        'BOILERPLATE:',boilerplate,'\n',
-        'SOUP:',soup,'\n',
-        'NOODLES:',noodles,'\n',
-        'NOODLE:',noodle,'\n'
-      )
-      if (!path) var path = repoName
-
-      console.log(`creating ${boilerplate} boilerplate at ${path}`)
-      git.clone(noodle, jetpack.cwd())
-      process.exit()
-    }
-  })
-
-niru.parse(process.argv)
+console.log(args)
